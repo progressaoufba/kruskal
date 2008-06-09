9a80eb6573309735c41610bbd2d905ecaaf336a2
tomyhayes <tomyhayes@1c43d8b8-3b4f-0410-9b3e-9175f793d891>
2008-06-09 15:30:00 +0000

    saque foo

---
+++ union_find.c
@@ -0,0 +1,36 @@
+#include "foresta.h"
+
+struct sunion_find {
+	tufset *rep;
+	int max_size;
+};
+
+union_find
+uf_create(const tufpos max) {
+	union_find uf;
+	uf = (union_find)calloc(1,sizeof(struct sunion_find));
+	if (uf!=NULL) {
+		uf->rep = (tufset)calloc((size_t)max,sizeof tufset);
+		if (uf->rep!=NULL) {
+			for (i=0,i<=max_size,i++)
+				uf->rep[i] = -1;
+			uf->max_size = max;
+		}
+	}
+}
+
+void
+uf_add_singulete(union_find uf, const tufalpha el) {}
+
+tufset
+uf_find(union_find uf, const tufalpha el) {
+	tufalpha aux;
+	aux = el;
+	if (uf!=NULL)
+		if (uf->rep!=NULL)
+			while (uf->rep[aux]>=0)
+				aux = rep[aux];
+	return 
+		
+		
+		
