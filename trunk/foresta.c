933e1acd61a21833e5568226ece9107754aa0ed6
tomyhayes <tomyhayes@1c43d8b8-3b4f-0410-9b3e-9175f793d891>
2008-06-09 15:22:10 +0000

    pwp

--- trunk/foresta.c
+++
@@ -1,9 +0,0 @@
-#include "foresta.h"
-
-
-union_find
-uf_create(const tufpos max) {
-	union_find uf;
-	uf = (union_find)calloc(1,sizeof (struct sunion_find));
-	uf->rep = ()calloc(max,sizeof int);
-	
