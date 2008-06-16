dd6c146abd76afb2cf85c6eb8638e1a3211975ec
tomyhayes <tomyhayes@1c43d8b8-3b4f-0410-9b3e-9175f793d891>
2008-06-16 03:41:24 +0000

    cambie los nombres de las funciones de stack a ingles..

--- trunk/stack.h
+++ trunk/stack.h
@@ -11,19 +11,17 @@ stack
 stack_create(const size_t maxt); /*constructor con maximo del tamaño */
 
 stack
-stack_apilar(stack s,const stalpha a);/*agregamos un elemento*/
-
+stack_push(stack s,const stalpha a);/*agregamos un elemento*/
 
 bool
-stack_vacia (const stack s);
+stack_empty(const stack s);
 
-
-/*generamos memoria, tener en cuenta que hay q borrarla*/
 stalpha
 stack_top(const stack s);/*devolvemos el de mas arriva*/
+/*generamos memoria, tener en cuenta que hay q borrarla*/
 
 stack
-stack_desapilar (stack s);/*sacamos el de mas arriva*/
+stack_pop (stack s);/*sacamos el de mas arriva*/
 
 /*vamos a eliminar todos los elementos que contiene el stack y despues lo liberamos*/
 stack
