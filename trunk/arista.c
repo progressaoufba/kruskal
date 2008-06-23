e2ccfdf5dd9bd54080910c82e90e32da6ed3a249
tomyhayes <tomyhayes@1c43d8b8-3b4f-0410-9b3e-9175f793d891>
2008-06-23 03:59:38 +0000

    el error estaba en que no se hacian thalpha_clone con el argumento de heap_insert

--- trunk/arista.c
+++ trunk/arista.c
@@ -43,7 +43,6 @@ arista
 arista_clone(arista e) {
 	if (e==NULL)
 		errx(EXIT_FAILURE, "Error al crear arista (argumento NULL), archivo %s, linea %i", __FILE__, __LINE__);
-	
 	return arista_create(e->v1,e->v2,e->weight);
 }
 
